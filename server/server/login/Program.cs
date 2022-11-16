55d9dd3b8b17a86ad11655beeae1e2562e1d5388
wuming <wuming@lilith.com>
2022-11-16 11:31:32 +0800

    login player

---
+++ server/server/login/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace login
+{
+    class Program
+    {
+
+
+		static void Main(string[] args)
+		{
+            var _hub = new hub.hub(args[0], args[1], "login");
+
+            log.log.trace("player start ok");
+
+            _hub.run();
+
+            log.log.info("server closed, player server:{0}", hub.hub.name);
+        }
+	}
+}
---
+++ server/server/player/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace player
+{
+    class Program
+    {
+
+
+		static void Main(string[] args)
+		{
+            var _hub = new hub.hub(args[0], args[1], "player");
+
+            log.log.trace("player start ok");
+
+            _hub.run();
+
+            log.log.info("server closed, player server:{0}", hub.hub.name);
+        }
+	}
+}
