f140382d0ca88dd692393caf7a0a1691cfba245f
wuming <wuming@lilith.com>
2022-11-16 11:12:48 +0800

    center_svr dbproxy_svr

---
+++ server/server/dbproxy_svr/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace dbproxy_svr
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{
+            var _dbproxy = new dbproxy.dbproxy(args[0], args[1]);
+
+            log.log.trace("dbproxy start ok");
+
+            _dbproxy.run();
+
+            log.log.info("server closed, dbproxy server:{0}", dbproxy.dbproxy.name);
+        }
+	}
+}
