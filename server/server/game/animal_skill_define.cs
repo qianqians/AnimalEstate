36a71441ce72b436d262747f753ac4791e1317ad
qianqians <theDarkforce@aliyun.com>
2022-11-27 23:01:34 +0800

    skill独立在一个文件定义

---
+++ server/server/game/animal_skill_define.cs
@@ -0,0 +1,215 @@
+﻿using abelkhan;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Security.Principal;
+
+namespace game
+{
+    partial class client_proxy
+    {
+
+        private void chicken_skill()
+        {
+            if (rounds < 3)
+            {
+                log.log.err($"skill rounds not achieve player.name:{_info.name} player.guid:{_info.guid}");
+                return;
+            }
+
+            if (skill_rounds < 6)
+            {
+                log.log.err($"skill in cd player.name:{_info.name} player.guid:{_info.guid}");
+                return;
+            }
+
+            skill_rounds = 0;
+
+            var _effect = new skill_effect();
+            _effect.move_coefficient = 1.5f;
+            _effect.continued_rounds = 4;
+            skill_Effects.Add(_effect);
+        }
+
+        private void monkey_skill()
+        {
+            if (rounds < 3)
+            {
+                log.log.err($"skill rounds not achieve player.name:{_info.name} player.guid:{_info.guid}");
+                return;
+            }
+
+            if (skill_rounds < 6)
+            {
+                log.log.err($"skill in cd player.name:{_info.name} player.guid:{_info.guid}");
+                return;
+            }
+
+            skill_rounds = 0;
+
+            var _effect = new skill_effect();
+            _effect.move_coefficient = 1.5f;
+            _effect.is_immunity = true;
+            _effect.continued_rounds = 3;
+            skill_Effects.Add(_effect);
+        }
+
+        private void rabbit_skill()
+        {
+            if (rounds < 3)
+            {
+                log.log.err($"skill rounds not achieve player.name:{_info.name} player.guid:{_info.guid}");
+                return;
+            }
+
+            if (skill_rounds < 6)
+            {
+                log.log.err($"skill in cd player.name:{_info.name} player.guid:{_info.guid}");
+                return;
+            }
+
+            skill_rounds = 0;
+
+            var _effect = new skill_effect();
+            _effect.dice_num = 2;
+            _effect.continued_rounds = 2;
+            skill_Effects.Add(_effect);
+        }
+
+        private void duck_skill()
+        {
+            if (rounds < 10)
+            {
+                log.log.err($"skill rounds not achieve player.name:{_info.name} player.guid:{_info.guid}");
+                return;
+            }
+
+            var _effect = new skill_effect();
+            _effect.move_coefficient = 2;
+            _effect.continued_rounds = int.MaxValue;
+            skill_Effects.Add(_effect);
+        }
+
+        private void mouse_skill()
+        {
+            if (rounds < 3)
+            {
+                log.log.err($"skill rounds not achieve player.name:{_info.name} player.guid:{_info.guid}");
+                return;
+            }
+
+            if (skill_rounds < 6)
+            {
+                log.log.err($"skill in cd player.name:{_info.name} player.guid:{_info.guid}");
+                return;
+            }
+
+            skill_rounds = 0;
+
+            var _effect = new effect_info();
+            _effect.effect_id = effect.mouse_trap;
+            _effect.grids = new List<short> { _game_info.current_pos };
+            _impl.effect_list.Add(_effect);
+        }
+
+        private void do_bear_skill()
+        {
+            PlayerGameInfo.current_pos -= 4;
+            if (PlayerGameInfo.current_pos < 0)
+            {
+                PlayerGameInfo.current_pos = 0;
+            }
+        }
+
+        private void bear_skill()
+        {
+            if (rounds < 3)
+            {
+                log.log.err($"skill rounds not achieve player.name:{_info.name} player.guid:{_info.guid}");
+                return;
+            }
+
+            if (skill_rounds < 6)
+            {
+                log.log.err($"skill in cd player.name:{_info.name} player.guid:{_info.guid}");
+                return;
+            }
+
+            skill_rounds = 0;
+
+            foreach (var _client in _impl.ClientProxys)
+            {
+                if (_client.PlayerGameInfo.guid != _game_info.guid)
+                {
+                    _client.do_bear_skill();
+                }
+            }
+        }
+
+        private void do_tiger_skill()
+        {
+            var _effect = new skill_effect();
+            _effect.could_move = false;
+            _effect.continued_rounds = 2;
+            skill_Effects.Add(_effect);
+        }
+
+        private void tiger_skill()
+        {
+            if (rounds < 3)
+            {
+                log.log.err($"skill rounds not achieve player.name:{_info.name} player.guid:{_info.guid}");
+                return;
+            }
+
+            if (skill_rounds < 6)
+            {
+                log.log.err($"skill in cd player.name:{_info.name} player.guid:{_info.guid}");
+                return;
+            }
+
+            skill_rounds = 0;
+
+            foreach (var _client in _impl.ClientProxys)
+            {
+                if (_client.PlayerGameInfo.guid != _game_info.guid)
+                {
+                    _client.do_tiger_skill();
+                }
+            }
+        }
+
+        private void do_lion_skill()
+        {
+            var _effect = new skill_effect();
+            _effect.move_coefficient = 0.5f;
+            _effect.continued_rounds = 3;
+            skill_Effects.Add(_effect);
+        }
+
+        private void lion_skill()
+        {
+            if (rounds < 3)
+            {
+                log.log.err($"skill rounds not achieve player.name:{_info.name} player.guid:{_info.guid}");
+                return;
+            }
+
+            if (skill_rounds < 6)
+            {
+                log.log.err($"skill in cd player.name:{_info.name} player.guid:{_info.guid}");
+                return;
+            }
+
+            skill_rounds = 0;
+
+            foreach (var _client in _impl.ClientProxys)
+            {
+                if (_client.PlayerGameInfo.guid != _game_info.guid)
+                {
+                    _client.do_lion_skill();
+                }
+            }
+        }
+
+    }
+}
