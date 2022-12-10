42361c7441fe652f910afcbc6ee2da6fa1485e48
吴铭 <451517996@qq.com>
2022-12-10 21:13:02 +0800

    匹配逻辑

--- AnimalEstate/assets/script/main_scene/match_interface.ts
+++ AnimalEstate/assets/script/main_scene/match_interface.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, Button, Sprite, director } from 'cc';
+import { _decorator, Component, Node, Button, Sprite, Label, director } from 'cc';
 const { ccclass, property } = _decorator;
 
 import * as singleton from '../netDriver/netSingleton';
@@ -8,12 +8,19 @@ import { playground } from '../serverSDK/common';
 export class match_interface extends Component {
     @property(Button)
     start_match_game:Button = null;
+    @property(Button)
+    left:Button = null;
+    @property(Button)
+    right:Button = null;
+    @property(Label)
+    countdown:Label = null;
     @property(Sprite)
     random_sprite:Sprite = null;
     @property(Sprite)
     lakside_sprite:Sprite = null;
 
     enum_playground:playground = playground.random;
+    countdown_count:number = 5;
 
     set_thumbnail() {
         this.random_sprite.node.active = false;
@@ -32,10 +39,16 @@ export class match_interface extends Component {
     start() {
         this.set_thumbnail();
 
+        this.countdown.node.active = false;
+
         this.start_match_game.node.on(Node.EventType.MOUSE_DOWN, this.start_match_game_callback, this);
+        this.left.node.on(Node.EventType.MOUSE_DOWN, this.left_callback, this);
+        this.right.node.on(Node.EventType.MOUSE_DOWN, this.right_callback, this);
 
         singleton.netSingleton.game.cb_game_wait_start_info = () => {
             console.log("start match!");
+
+            this.unschedule(this.countdown_callback);
 
             if (singleton.netSingleton.game.Playground == playground.lakeside){
                 director.loadScene("lakeside_game");
@@ -48,8 +61,31 @@ export class match_interface extends Component {
     update(deltaTime: number) {
     }
     
+    countdown_callback() {
+        this.countdown.string = (--this.countdown_count).toString();
+    }
+
     start_match_game_callback() {
         singleton.netSingleton.match.start_match(this.enum_playground);
+
+        this.countdown.node.active = true;
+        this.schedule(this.countdown_callback, 1);
+    }
+
+    left_callback() {
+        this.enum_playground--;
+        if (this.enum_playground < 0) {
+            this.enum_playground = playground.lakeside;
+        }
+        this.set_thumbnail();
+    }
+
+    right_callback() {
+        this.enum_playground++;
+        if (this.enum_playground > playground.lakeside) {
+            this.enum_playground = playground.random;
+        }
+        this.set_thumbnail();
     }
 }
 
